ce25becb36b8e5829d2089855194c57876799c35
jeanpierrethach <jeanpierre.thach@gmail.com>
2017-11-25 09:45:49 -0500

    Added summary component

--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/MainActivity.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/MainActivity.java
@@ -3,11 +3,25 @@ package com.strudelauxpommes.fitnesshabits;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.strudelauxpommes.fitnesshabits.physicalactivity.PhysicalActivityFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentTransaction fragmentTransaction;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.rl, new PhysicalActivityFragment());
+        fragmentTransaction.commit();
+
+        setContentView(R.layout.fragment_physical_activity);
     }
 }
